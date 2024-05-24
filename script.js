eb02db689a6fa1b8caa46ebba6ae2cc4a518655a
Dany Arya Maulana <149315575+danyaryam@users.noreply.github.com>
2024-05-24 20:21:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const title = document.querySelector('.title')
+const leaf1 = document.querySelector('.leaf1')
+const leaf2 = document.querySelector('.leaf2')
+const bush2 = document.querySelector('.bush2')
+const mount1 = document.querySelector('.mount1')
+const mount2 = document.querySelector('.mount2')
+
+document.addEventListener('scroll', function() {
+    let value = window.scrollY
+    // console.log(value)
+    title.style.marginTop = value * 1.1 + 'px'
+
+    leaf1.style.marginLeft = -value + 'px'
+    leaf2.style.marginLeft = value + 'px'
+
+    bush2.style.marginBottom = -value + 'px'
+
+    mount1.style.marginBottom = -value * 1.1 + 'px'
+    mount2.style.marginBottom = -value * 1.2 + 'px'
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.querySelector('.navbar');
+    let lastScrollTop = 0;
+
+    window.addEventListener('scroll', function () {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            // discroll kebawah
+            navbar.classList.add('navbar-hidden');
+            navbar.classList.remove('navbar-visible');
+        } else {
+            // discroll ke atas
+            navbar.classList.remove('navbar-hidden');
+            navbar.classList.add('navbar-visible');
+        }
+
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+    });
+});
